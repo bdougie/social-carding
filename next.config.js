ba0c699be1591086160ffea345bde21d140a9f5a
Brian 'bdougie' Douglas <brian@opensauced.pizza>
2025-03-21 19:16:43 -0700

    updated

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  // Remove any deprecated options like 'target', 'webpack5', etc.
+  // Add new required configurations
 }
+
+module.exports = nextConfig
